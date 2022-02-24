0cc7e280ecff2be1d81780ab445da7db21b7241d
Manishankar Vakta <manishankarvakta@gmail.com>
2022-02-24 13:59:11 +0600

    new transaction test done

--- dev/test.js
+++ dev/test.js
@@ -2,7 +2,19 @@ const Blockchain = require('./blockchain');
 
 const bdcoin = new Blockchain();
 bdcoin.createNewBlock(1532,'JLDF556dvsvJ', 'KJSGDV23daAD');
-bdcoin.createNewBlock(2452,'derfg6s4g3', 'sdfgsdf3sdf6');
-bdcoin.createNewBlock(345,'sdfgsdfg26131', 'sdfgsdfg13sdf');
-bdcoin.createNewBlock(3562,'sdfgsdfg561', 'sdfgd51v68dzf4v');
+
+bdcoin.createNewTransaction(100,'MANIJLDF556dSHdJ', 'ATIQKJSGD23dDFAD');
+
+bdcoin.createNewBlock(15562,'JLDF556dSDvJ', 'KJSGDV23dSFAD');
+
+bdcoin.createNewTransaction(50,'MANIJLDF556dSHdJ', 'ATIQKJSGD23dDFAD');
+bdcoin.createNewTransaction(30,'MANIJLDF556dSHdJ', 'ATIQKJSGD23dDFAD');
+bdcoin.createNewTransaction(120,'MANIJLDF556dSHdJ', 'ATIQKJSGD23dDFAD');
+
+bdcoin.createNewBlock(23462,'JLDF556dSDDJ', 'KJSGDV2SAdSFAD');
+ 
 console.log(bdcoin);
+console.log('--------');
+console.log(bdcoin.chain[1]);
+console.log('--------');
+console.log(bdcoin.chain[2]);
