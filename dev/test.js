e2b6690fed7876ecff99fcf1c4b5f5a8a73971f5
Manishankar Vakta <manishankarvakta@gmail.com>
2022-02-24 23:20:04 +0600

    Genesis Block

--- dev/test.js
+++ dev/test.js
@@ -2,29 +2,4 @@ const Blockchain = require('./blockchain');
 
 const bdcoin = new Blockchain();
 
-
-const previousBlockHash = 'WIOVJIV4F66V4ZDFBSZD3F';
-const currentBlockData = [
-    {
-        amount: 102,
-        sender: 'SDGVAFDIHV55DFV23DD',
-        recipient: 'SDJFODFVH4SD4CS6'
-    },
-    {
-        amount: 150,
-        sender: 'SDGVAFDIHV55DFV23DD',
-        recipient: 'SDJFODFVH4SD4CS6'
-    },
-    {
-        amount: 250,
-        sender: 'SDGVAFDIHV55DFV23DD',
-        recipient: 'SDJFODFVH4SD4CS6'
-    }
-];
-
-
-const nonce = bdcoin.proofOfWork(previousBlockHash, currentBlockData);
-
-console.log(nonce);
-
-console.log(bdcoin.hashBlock(previousBlockHash, currentBlockData, 42066));
+console.log(bdcoin)
