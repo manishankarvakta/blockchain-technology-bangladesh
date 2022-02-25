a662612fe7c3f51373f6b593e48d942bd3283e38
Manishankar Vakta <manishankarvakta@gmail.com>
2022-02-25 09:20:13 +0600

    register-and-broadcast-node

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -3,6 +3,8 @@ const app = express()
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const uuid = require('uuid');
+const port = process.argv[2];
+const rp = require('request-promise');
 
 const nodeAddress = uuid.v1().split('-').join('');
 
@@ -47,6 +49,45 @@ app.get('/mine', function (req, res) {
 })
 
 
-app.listen(3000, function(){
-  console.log('Listening on: http://localhost:3000')
+app.post('/register-and-broadcast-node', function(req, res){
+  const newNodeUrl = req.body.newNodeUrl;
+  if(bdcoin.networkNodes.indexOf(newNodeUrl) == -1) bdcoin.networkNodes.push(newNodeUrl);
+  
+  const regNodesPromises = [];
+  
+  bdcoin.networkNodes.forEach(networkNodeUrl =>{
+    const requestOptions = {
+      uri: networkNodeUrl + '/register-node',
+      method: 'POST',
+      body: { newNodeUrl: newNodeUrl},
+      json: true
+    };
+    regNodesPromises.push(rp(requestOptions));
+  });
+  Promise.all(regNodesPromises)
+  .then(data => {
+    const bulkRegisterOptions = {
+      uri: newNodeUrl+'register-node-bulk',
+      method: 'POST',
+      body: {allNetworkNodes: [...bdcoin.networkNodes, bdcoin.currentNodeUrl] },
+      json: true
+    }
+    rp(bulkRegisterOptions);
+  })
+  .then(data => {
+    res.json({ note: 'New Node registered with network successfully'})
+  })
+});
+
+
+app.post('/register-node', function(req, res){
+
+});
+
+app.post('/register-node-bulk', function(req, res){
+
+});
+
+app.listen(port, function(){
+  console.log(`Listening on: http://localhost:${port}`)
 })
