e67dfa11c015a51afd7d022555e95575878b5332
Manishankar Vakta <manishankarvakta@gmail.com>
2022-02-28 01:03:38 +0600

    getBlock Method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -92,4 +92,14 @@ Blockchain.prototype.chainIsValid = function(blockchain){
     return validChain;
 };
 
+// get block
+Blockchain.prototype.getBlock = function(blockHash) {
+    let correctBlock = null;
+    this.chain.forEach(block =>{
+        if(block.hash === blockHash) correctBlock = block;
+    });
+    return correctBlock;
+}
+
+
 module.exports = Blockchain;
